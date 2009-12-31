fe9be68ed9ae8399b7c7c94a9249c1f664a56765
michele.avellino <michele.avellino@55ca7f0a-be8f-11de-bb5c-85734917f5ce>
2009-12-31 13:42:18 +0000

    Fixing limit 50 in queries to retrieve store for graphs

--- src/it/fub/jardin/server/ManagerServiceImpl.java
+++ src/it/fub/jardin/server/ManagerServiceImpl.java
@@ -152,11 +152,14 @@ public class ManagerServiceImpl extends RemoteServiceServlet implements
   }
 
   public PagingLoadResult<BaseModelData> getRecords(PagingLoadConfig config,
-      SearchParams searchParams) throws HiddenException {
+      SearchParams searchParams, boolean limit) throws HiddenException {
     List<BaseModelData> records = dbUtils.getObjects(config, searchParams);
 
     if (config != null) {
       int recordSize = dbUtils.countObjects(searchParams);
+      if (!limit){
+        config.setLimit(0);
+      }
       return new BasePagingLoadResult<BaseModelData>(records,
           config.getOffset(), recordSize);
     } else {
